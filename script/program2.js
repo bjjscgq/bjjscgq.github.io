7ca132ac5d8dea0494e6d23a138c157601cfaa02
bjjscgq <bjjscgq@163.com>
2021-08-30 12:33:26 +0800

    Signed-off-by: bjjscgq <bjjscgq@163.com>

--- script/program2.js
+++ script/program2.js
@@ -1,15 +1,16 @@
 let requestUrl = "json/大富翁.json";
 let request = new XMLHttpRequest();
 request.open('GET', requestUrl);
-request.send(null);/*不发送数据到服务器*/
+request.send(null); /*不发送数据到服务器*/
 var json;
-request.onload = function () {/*XHR对象获取到返回信息后执行*/
-    if (request.status == 200) {/*返回状态为200，即为数据获取成功*/
+request.onload = function() { /*XHR对象获取到返回信息后执行*/
+    if (request.status == 200) { /*返回状态为200，即为数据获取成功*/
         json = JSON.parse(request.responseText);
     }
 }
-function find(){
-    var index = parseInt(document.getElementById("in").value)-1;
+
+function find() {
+    var index = parseInt(document.getElementById("in").value) - 1;
     document.getElementById("question").value = json.daFuWeng[index].question;
     document.getElementById("answer").value = json.daFuWeng[index].answer;
 }
