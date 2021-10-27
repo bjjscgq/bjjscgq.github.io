a23e9a5980ed20d25f208be7a525a2da245827ce
bjjscgq <bjjscgq@163.com>
2021-10-27 21:47:49 +0800

    Signed-off-by: bjjscgq <bjjscgq@163.com>

--- script/program3.js
+++ script/program3.js
@@ -4,10 +4,11 @@ let request = new XMLHttpRequest();
 request.open('GET', requestUrl);
 request.send(null); /*不发送数据到服务器*/
 var json;
+var words;
 request.onload = function() { /*XHR对象获取到返回信息后执行*/
     if (request.status == 200) { /*返回状态为200，即为数据获取成功*/
         json = JSON.parse(request.responseText);
-        var words = json.words;
+        words = json.words;
     }
 }
 //窗口尺寸改变响应（修改canvas大小）
